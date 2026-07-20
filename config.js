a38549655578cd9c2b243e92b1058ce1e75eeac3
nglequocan2005-jpg <nglequocan2005@gmail.com>
2026-07-20 16:05:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const API_URL="https://script.google.com/macros/s/AKfycbxFwfr43uTT9jiDiiP32paFjR4NqT8qM503Cpdk7TS5L0ebjeeV6ABxDCHi2L9dbgpw8A/exec";
+const API_URL="https://script.google.com/macros/s/AKfycbyOPGI9iaGT25NH7rVKaskrJey1yY9-JKBUbabIO85DjEfFspkpoEQMIWIFt1gF5yhQ7g/exec";
